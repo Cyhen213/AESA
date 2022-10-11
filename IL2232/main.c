8f0d03f5c562dc4e5d34ad0678b0c05b61981435
Cyhen213 <yuchenga@kth.se>
2022-10-11 10:16:44 +0200

    Initial Commit

---
+++ IL2232/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  IL2232
+//
+//  Created by gaogao on 2022-10-11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
